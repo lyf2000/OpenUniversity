7bd77a871e2d755c618765edd4fc89cbd77918cc
Raynur <raynur.khasanov@gmail.com>
2019-04-27 22:42:36 +0300

    Добавил шаблоны, во views их обрабтал , в urls изменения

--- ttest/views.py
+++ ttest/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test(request):
+    return render(request, 'ttest/tests.html')
+
+def test_result(request):
+    return render(request, 'ttest/resultsTests.html')
