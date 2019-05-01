d410094a7f015465c13063bbfa9b9655ea1e4780
lyf2000 <lyf2000@mail.ru>
2019-05-01 09:52:13 +0300

    deleted CustomUser

--- signin_signup/models.py
+++ signin_signup/models.py
@@ -3,8 +3,3 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class CustomUser(User):
-	data_joined = models.DateTimeField()
-
-	def __str__(self):
-		return f'{self.username}'
